c4e37c101826b1735b69cced38f2b2efc568583e
coletistan <coletistan@gmail.com>
2024-07-19 20:47:41 -0400

    Added cors package.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const elementsRouter = require("./elements/elements.routes");
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(elementsRouter);
 
 app.listen(port, () => {
-    console.info(`Running on http://localhost:${port}...`);
+  console.info(`Running on http://localhost:${port}...`);
 });
